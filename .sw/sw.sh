7f5ff3420701f6f49ecb9755becc0df0acbb01a5
Chintan Patel <chintan@trialx.com>
2020-06-20 01:06:35 -0400

    i3 config, local utils and installed sw

---
+++ .sw/sw.sh
@@ -0,0 +1,24 @@
+#python stuff
+sudo apt-get install build-essential cmake pkg-config
+sudo apt-get install python-dev python-pip python3-dev python3-pip
+
+#stream from dslr and ffmpeg
+sudo apt-get install gphoto2 v4l2loopback-utils ffmpeg
+
+#softwares
+sudo apt-get install vlc
+
+#brightness night light
+sudo apt-get install redshift
+sudo apt install brightnessctl
+
+#audio
+sudo apt install pavucontrol
+sudo apt install pulseaudio-module-bluetooth
+sudo apt-get install volumeicon-alsa
+
+#gnome stuff
+sudo apt install gnome-tweak
+
+#cmdline explorer
+sudo apt install ranger
